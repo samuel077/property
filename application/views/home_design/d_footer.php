c5ef83024b6c149cff47d7bb87cc31d70b46c8fe
Tutu <hytina7580@gmail.com>
2014-09-02 16:52:19 +0800

    change route to let 'design' as the homepage

--- application/views/home_design/d_footer.php
+++ application/views/home_design/d_footer.php
@@ -2,7 +2,7 @@
 </div> <!-- end of fixed container -->
 <p style="width:100%; color:white; text-align:center; position:absolute; bottom :10px; z-index:1">
 2014 &nbsp; © HSNG(ANT)財產管理網頁 &nbsp;<br>
-designed by : hungo, tutu, Samuel </p>
+designed by : birdannoyed, hungo, tutu, Samuel </p>
 </div>
 </body>
 </html>
